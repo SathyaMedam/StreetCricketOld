03ad93b6e177efaf0dd589af749ad8db50f61009
Sathya <sathi.dude@gmail.com>
2018-04-09 05:27:59 +0100

    Run Out

--- StreetCricket/StreetCricket/StreetCricket/Views/Scoring.xaml.cs
+++ StreetCricket/StreetCricket/StreetCricket/Views/Scoring.xaml.cs
@@ -14,6 +14,12 @@ namespace StreetCricket.Views
     public partial class Scoring : ContentPage
     {
         public List<SelectableData<SelectPlayerData>> DataList { get; set; }
+        public List<SelectPlayerData> PickRunsType { get; set; }
+        public List<CricketPlayer> FieldingTeamPlayers { get; set; }
+        public List<CricketPlayer> BattingTeamPlayers { get; set; }
+        public CricketPlayer BatsmenOut { get; set; }
+        public CricketPlayer Fielder { get; set; }
+        public RunsType SelectdRunsType { get; set; }
         private readonly CricketMatch _cricketMatch;
 
         public Scoring()
@@ -72,7 +78,9 @@ namespace StreetCricket.Views
             LblAwayTeamRuns.Text = awayScoredCard.TotalRuns.ToString();
             LblAwayTeamWickets.Text = awayScoredCard.Wickets.ToString();
             LblAwayTeamOvers.Text = awayScoredCard.Overs;
-            var batsmen1ScoreCard = _cricketMatch.GetBattingScoreCard(true, 1, _cricketMatch.CurrentInnings.Striker.Id);
+            if (_cricketMatch.CurrentInnings.Striker!=null)
+            {
+                var batsmen1ScoreCard = _cricketMatch.GetBattingScoreCard(true, 1, _cricketMatch.CurrentInnings.Striker.Id);
             LblStrikerName.Text = _cricketMatch.CurrentInnings.Striker.Name;
             LblStrikerRuns.Text = batsmen1ScoreCard.TotalRuns.ToString();
             LblStrikerBalls.Text = batsmen1ScoreCard.BallsFaced.ToString();
@@ -80,14 +88,19 @@ namespace StreetCricket.Views
             LblStrikerSixers.Text = batsmen1ScoreCard.NumberOfSixers.ToString();
             LblStrikerZeros.Text = batsmen1ScoreCard.NumberOfDotBalls.ToString();
             LblStrikerStrikeRate.Text = batsmen1ScoreCard.StrikeRate.ToString(CultureInfo.InvariantCulture);
-            var batsmen2ScoreCard = _cricketMatch.GetBattingScoreCard(true, 1, _cricketMatch.CurrentInnings.NonStriker.Id);
-            LblNonStrikerName.Text= _cricketMatch.CurrentInnings.NonStriker.Name;
-            LblNonStrikerRuns.Text = batsmen2ScoreCard.TotalRuns.ToString();
-            LblNonStrikerBalls.Text = batsmen2ScoreCard.BallsFaced.ToString();
-            LblNonStrikerFours.Text = batsmen2ScoreCard.NumberOfFours.ToString();
-            LblNonStrikerSixers.Text = batsmen2ScoreCard.NumberOfSixers.ToString();
-            LblNonStrikerZeros.Text = batsmen2ScoreCard.NumberOfDotBalls.ToString();
-            LblNonStrikerStrikeRate.Text = batsmen2ScoreCard.StrikeRate.ToString(CultureInfo.InvariantCulture);
+            }
+
+            if (_cricketMatch.CurrentInnings.NonStriker != null)
+            {
+                var batsmen2ScoreCard =_cricketMatch.GetBattingScoreCard(true, 1, _cricketMatch.CurrentInnings.NonStriker.Id);
+                LblNonStrikerName.Text = _cricketMatch.CurrentInnings.NonStriker.Name;
+                LblNonStrikerRuns.Text = batsmen2ScoreCard.TotalRuns.ToString();
+                LblNonStrikerBalls.Text = batsmen2ScoreCard.BallsFaced.ToString();
+                LblNonStrikerFours.Text = batsmen2ScoreCard.NumberOfFours.ToString();
+                LblNonStrikerSixers.Text = batsmen2ScoreCard.NumberOfSixers.ToString();
+                LblNonStrikerZeros.Text = batsmen2ScoreCard.NumberOfDotBalls.ToString();
+                LblNonStrikerStrikeRate.Text = batsmen2ScoreCard.StrikeRate.ToString(CultureInfo.InvariantCulture);
+            }
         }
 
         private void On0RunClicked(object sender, EventArgs e)
@@ -125,8 +138,7 @@ namespace StreetCricket.Views
 
         private void OnWicketClicked(object sender, EventArgs e)
         {
-            AddDismissalOfBall(0, 0, BallType.Legitimate, _cricketMatch.CurrentInnings.Striker, _cricketMatch.CurrentInnings.CurrentOver.Bowler,
-                DisMissalType.Caught);
+            LayoutWickets.IsVisible = true;
         }
 
         private void OnWideClicked(object sender, EventArgs e)
@@ -174,11 +186,10 @@ namespace StreetCricket.Views
             //    _cricketMatch.CurrentInnings.CurrentOver.Bowler, _cricketMatch.CurrentInnings.Striker, false, null, null, DisMissalType.None);
 
         }
-        private void AddDismissalOfBall(int runsScored, int extrasScored, BallType ballType, CricketPlayer dismissedPlayer, CricketPlayer feilder, DisMissalType disMissalType)
+        private void AddDismissalOfBall(int runsScored, BallType ballType, RunsType runsType, CricketPlayer dismissedPlayer, CricketPlayer feilder, DisMissalType disMissalType)
         {
-            //var runs = new Runs { RunsScored = runsScored, Extras = extrasScored };
-            //_cricketMatch.AddBall(_cricketMatch.CurrentInnings.CurrentOver, ballType, runs,
-            //    _cricketMatch.CurrentInnings.CurrentOver.Bowler, _cricketMatch.CurrentInnings.Striker, true, dismissedPlayer, feilder, disMissalType);
+            _cricketMatch.AddBall(_cricketMatch.CurrentInnings.CurrentOver, ballType, runsType, BoundaryType.None, runsScored,
+                _cricketMatch.CurrentInnings.CurrentOver.Bowler, _cricketMatch.CurrentInnings.Striker, true, dismissedPlayer, feilder, disMissalType);
             UpdateScoreBoard();
         }
         private void OnWide0Clicked(object sender, EventArgs e)
@@ -250,6 +261,105 @@ namespace StreetCricket.Views
             var selectedPlayerId = DataList.Where(x => x.Selected).ToList().FirstOrDefault().Data.Id;
             _cricketMatch.StartOver(_cricketMatch.CurrentInnings, selectedPlayerId);
             LayoutBowler.IsVisible = false;
+        }
+
+        private void OnRunOutClicked(object sender, EventArgs e)
+        {
+            LayoutRunOut.IsVisible = true;
+            var runsType = new List<SelectPlayerData>
+            {
+                new SelectPlayerData{Id = 1, Name ="Runs" },
+                new SelectPlayerData{Id = 2, Name ="Byes" },
+                new SelectPlayerData{Id = 3, Name ="LegByes" },
+                new SelectPlayerData{Id = 4, Name ="Wides" } ,
+                new SelectPlayerData{Id = 5, Name ="NoBall" }
+
+            };
+            var strikerId = _cricketMatch.CurrentInnings.Striker;
+            var nonStrikerId = _cricketMatch.CurrentInnings.NonStriker;
+            PickerBatsmenOut.ItemsSource = BattingTeamPlayers = new List<CricketPlayer> { strikerId, nonStrikerId };
+            PickerRunsType.ItemsSource = PickRunsType = runsType;
+            PickerFielder.ItemsSource = FieldingTeamPlayers = _cricketMatch.CurrentInnings.BowlingTeam.Players.ToList();
+        }
+
+        private void StrikerOutClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnNonStrikerOutClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SwitchSelectBatsmen_OnToggled(object sender, ToggledEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Picker_OnBatsmenChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            BatsmenOut = (CricketPlayer)picker.SelectedItem;
+            if (BatsmenOut != null)
+            {
+                BtnRunOutConfirm.IsEnabled = true;
+            }
+
+        }
+
+        private void Picker_FielderChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            Fielder = (CricketPlayer)picker.SelectedItem;
+        }
+
+        private void Picker_OnRunsTypeChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            RunsType runsType = RunsType.Run;
+            var selectedIndex = (SelectPlayerData)picker.SelectedItem;
+            if (selectedIndex.Id == 1)
+            {
+                runsType = RunsType.Run;
+            }
+            else if (selectedIndex.Id == 2)
+            {
+                runsType = RunsType.Byes;
+            }
+            else if (selectedIndex.Id == 3)
+            {
+                runsType = RunsType.LegByes;
+            }
+            else if (selectedIndex.Id == 4)
+            {
+                runsType = RunsType.Wide;
+            }
+            else if (selectedIndex.Id == 5)
+            {
+                runsType = RunsType.NoBall;
+            }
+            SelectdRunsType = runsType;
+        }
+
+        private void OnRunOutConfirmClicked(object sender, EventArgs e)
+        {
+            var runs = stepper.Value;
+            var ballType = BallType.Legitimate;
+            if (SelectdRunsType == RunsType.Wide)
+            {
+                ballType = BallType.Wide;
+            }
+            else if (SelectdRunsType == RunsType.Wide)
+            {
+                ballType = BallType.NoBall;
+            }
+            AddDismissalOfBall((int)runs, ballType, SelectdRunsType, BatsmenOut, Fielder, DisMissalType.RunOut);
+            LayoutRunOut.IsVisible = false;
+            LayoutWickets.IsVisible = false;
+            LayoutMain.IsEnabled = false;
+
+
         }
     }
 }
